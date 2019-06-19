8870a4c980255074e95e47306852a5b99e077808
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-19 20:27:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,7 @@ class Song
   end
   
   def self.new_by_filename(file)
-    song = self.new 
-    song_info = file.chomp(".mp3").split(" - ")[1]
+    song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist = file.chomp(".mp3").split(" - ")[2]
     arist 
